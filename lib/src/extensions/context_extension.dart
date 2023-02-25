93366d8362472e63d9b3869a29bf63597817232f
Asta <ashtaaav@gmail.com>
2023-02-25 11:08:45 +0800

    context bug fixes

--- lib/src/extensions/context_extension.dart
+++ lib/src/extensions/context_extension.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:lazyui/lazyui.dart';
 
 extension ContextExtension on BuildContext {
   double get height => MediaQuery.of(this).size.height;
@@ -18,12 +21,19 @@ extension ContextExtension on BuildContext {
   }
 
   // show bottom sheet
-  Future<T?> bottomSheet<T extends Object?>(Widget widget, {bool dismiss = true, Color? backgroundColor, bool isScrollControlled = false}) {
+  Future<T?> bottomSheet<T extends Object?>(Widget widget,
+      {bool dismiss = true, bool useSafeArea = true, bool enableDrag = false, Color? backgroundColor, bool isScrollControlled = true}) {
+    Widget wrapper(Widget child) => Container(
+        padding: Ei.only(t: useSafeArea ? MediaQueryData.fromWindow(window).padding.top : 0),
+        decoration: BoxDecoration(color: backgroundColor ?? Colors.white),
+        child: child);
+
     return showModalBottomSheet<T>(
         context: this,
-        backgroundColor: backgroundColor ?? Colors.transparent,
+        backgroundColor: Colors.transparent,
         isDismissible: dismiss,
         isScrollControlled: isScrollControlled,
-        builder: ((context) => widget));
+        enableDrag: enableDrag,
+        builder: ((context) => wrapper(widget)));
   }
 }
