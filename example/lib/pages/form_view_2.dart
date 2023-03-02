dfe4ef2f1c9d8038a4c47cf6302796c278650097
Asta <ashtaaav@gmail.com>
2023-03-02 14:22:07 +0800

    stacked list view

--- example/lib/pages/form_view_2.dart
+++ example/lib/pages/form_view_2.dart
@@ -1,4 +1,3 @@
-import 'package:example/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:lazyui/lazyui.dart';
 
@@ -12,53 +11,27 @@ class FormView2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Form View'),
-      ),
-      body: StackedListView(
-          stacked: Button(
-            onTap: () {
-              context.dialog(CustomDialog(
-                tapClose: true,
-                alignment: Caa.start,
-                children: [
-                  Text('Dialog Title', style: Gfont.fs20.bold),
-                  Textr(
-                    Lipsum.createWord(15),
-                    margin: Ei.only(t: 10, b: 15),
-                  ),
-                  Col(
-                    children: List.generate(5, (i) {
-                      return Container(
-                        padding: Ei.sym(v: 20),
-                        decoration: BoxDecoration(border: Br.only(['t'])),
-                        child: Row(
-                          children: [
-                            Text(Lipsum.createWord(3)),
-                          ],
-                        ),
-                      );
-                    }),
-                  ),
-                  const TextInputTransparent(
-                    hint: 'Type something...',
-                  ),
-                ],
-              ));
-            },
-            text: 'Submit',
-          ),
-          children: [
-            FormsGroup(children: [
-              Forms.input(label: 'Email', hint: 'Input your email', controller: forms['email']),
-              Forms.input(label: 'Password', hint: 'Input your password', obsecureToggle: true, controller: forms['password']),
-            ]),
-            Forms.switches(
-              label: 'Available|Not Available',
-              onChanged: (value) {},
+    return Wrapper(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Form View'),
+        ),
+        body: StackedListView(
+            stacked: Button(
+              onTap: () {},
+              text: 'Submit',
             ),
-          ]),
+            children: [
+              FormsGroup(children: [
+                Forms.input(label: 'Email', hint: 'Input your email', controller: forms['email']),
+                Forms.input(label: 'Password', hint: 'Input your password', obsecureToggle: true, controller: forms['password']),
+              ]),
+              Forms.switches(
+                label: 'Available|Not Available',
+                onChanged: (value) {},
+              ),
+            ]),
+      ),
     );
   }
 }
--- lib/src/widgets/stacked_list_view.dart
+++ lib/src/widgets/stacked_list_view.dart
@@ -2,7 +2,10 @@ import 'dart:async';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:lazyui/lazyui.dart';
+
+class StackedListNotifier extends ChangeNotifier {}
 
 class StackedListView extends StatelessWidget {
   final List<Widget> children;
@@ -14,41 +17,61 @@ class StackedListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final streamController = StreamController<double>();
+    final opacityStreamController = StreamController<bool>();
     final childrenKey = GlobalKey(), stackedKey = GlobalKey();
 
     List<Widget> children = [];
+    double defaultHeight = -1;
+    bool isInit = false;
 
     initWidget() {
       children = [
         Col(key: childrenKey, children: [...this.children]),
         StreamBuilder<double>(
           stream: streamController.stream,
-          builder: (BuildContext context, snap) => SizedBox(height: snap.data ?? 0),
+          builder: (BuildContext context, snap) => AnimatedContainer(duration: const Duration(milliseconds: 150), height: snap.data ?? 0),
         ),
-        Container(key: stackedKey, child: stacked),
+        StreamBuilder<bool>(
+          stream: opacityStreamController.stream,
+          builder: (BuildContext context, snap) => AnimatedOpacity(
+              duration: const Duration(milliseconds: 300), opacity: snap.hasData ? 1 : 0, child: Container(key: stackedKey, child: stacked)),
+        ),
       ];
     }
 
-    onRender() {
-      // get children height
-      final RenderBox? box = childrenKey.currentContext?.findRenderObject() as RenderBox?;
-      double contentHeight = box?.size.height ?? 0;
+    onRender(bool visible) {
+      if (!visible) {
+        streamController.add(defaultHeight < 0 ? 0 : defaultHeight);
+        if (defaultHeight != -1) return;
+      }
 
-      // get stacked height
-      final RenderBox? box2 = stackedKey.currentContext?.findRenderObject() as RenderBox?;
-      double buttonHeight = box2?.size.height ?? 0;
+      Utils.timer(() {
+        // get children height
+        final RenderBox? box = childrenKey.currentContext?.findRenderObject() as RenderBox?;
+        double contentHeight = box?.size.height ?? 0;
 
-      // 56 = appbar height
-      double statusBarHeight = MediaQueryData.fromWindow(window).padding.top;
-      double content = contentHeight + buttonHeight + (padding * 2) + 56 + statusBarHeight;
+        // get stacked height
+        final RenderBox? box2 = stackedKey.currentContext?.findRenderObject() as RenderBox?;
+        double buttonHeight = box2?.size.height ?? 0;
 
-      double height = context.height - content;
-      streamController.add(height < 0 ? padding : height);
+        // 56 = appbar height
+        double statusBarHeight = MediaQueryData.fromWindow(window).padding.top;
+        double content = contentHeight + buttonHeight + (padding * 2) + 56 + statusBarHeight;
+
+        double height = (context.height - content) - MediaQueryData.fromWindow(window).viewInsets.bottom;
+        streamController.add(height < 0 ? padding : height);
+        if (defaultHeight == -1) defaultHeight = height;
+
+        opacityStreamController.add(isInit);
+      }, 250);
     }
 
     initWidget();
 
-    WidgetsBinding.instance.addPostFrameCallback((_) => onRender());
-    return ListView(physics: physics ?? BounceScroll(), padding: Ei.all(padding), children: children);
+    return KeyboardVisibilityBuilder(builder: (_, visible) {
+      WidgetsBinding.instance.addPostFrameCallback((_) => onRender(visible));
+
+      return ListView(physics: physics ?? BounceScroll(), padding: Ei.all(padding), children: children);
+    });
   }
 }
