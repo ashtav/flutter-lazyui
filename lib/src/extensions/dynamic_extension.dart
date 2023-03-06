0563dd0ad3249036d2ea368adffe48e783ab639f
Ashtav <ashtaaav@gmail.com>
2023-03-06 17:51:40 +0800

    bug fixes

--- lib/src/extensions/dynamic_extension.dart
+++ lib/src/extensions/dynamic_extension.dart
@@ -64,7 +64,7 @@ extension DynamicExtension on dynamic {
   /// name.orIf('-', [null, '']) // it's mean if name is null or empty, then return '-'
   /// ```
   ///
-  dynamic orIf([dynamic value, List conditions = const [null, '']]) {
+  T orIf<T>([dynamic value, List conditions = const [null, '']]) {
     Type type = runtimeType;
     dynamic result = '-';
 
