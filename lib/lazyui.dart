e0aa701099df3f0b2e8ba5f61e497b04ab566b1f
Ashta <33148617+ashtav@users.noreply.github.com>
2024-08-07 14:01:16 +0800

    New Update

--- lib/lazyui.dart
+++ lib/lazyui.dart
@@ -10,7 +10,6 @@ export 'package:cached_network_image/cached_network_image.dart';
 export 'package:device_info_plus/device_info_plus.dart';
 export 'package:flutter_svg/flutter_svg.dart';
 export 'package:google_fonts/google_fonts.dart';
-export 'package:http/http.dart';
 export 'package:intl/intl.dart';
 export 'package:path_provider/path_provider.dart';
 export 'package:stack_trace/stack_trace.dart';
